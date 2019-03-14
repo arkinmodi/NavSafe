5c203ef8b3d5d648bab6d4909a1a52f93ebd6feb
Benson Hall <hallb8@mcmaster.ca>
2019-03-14 16:37:54 -0400

    2 getters added - still incomplete

--- Group08_FinalProject/app/src/main/java/Collision.java
+++ Group08_FinalProject/app/src/main/java/Collision.java
@@ -4,30 +4,37 @@ import java.lang.reflect.Array;
  */
 public class Collision{
 
+	private float fXCoord;		//X-coordinate of collision location
+	private float fYCoord;		//Y-coordinate of a collision location
 	private String fColDesc;	//Collision description
 	private String fColType; 	//Collision type
 	private String fJuncType; 	//Junction type
-	private String fLightCond;
-	private String fRoadCond;
+	private String fLightCond;	//Light conditions
+	private String fRoadCond;	//Road conditions
 	private String fSevCode; 	//Severity code
 	private String fSevDesc;	//Severity description
-	private String fWeather;
-	private float fXCoord;
-	private float fYCoord;
+	private String fWeather;	//Weather conditions
+	private int fPedCount;		//pedestrian count
+	private int fInjuries; 		//number of injuries
+	private int fSInjuries;		//number of serious injuries
+	private int fFatalities;	//number of fatalities
+	
 	
 	/**
 	 * Constructor for an intersection
+	 * @param splitRead - array of collision data from read module
 	 */
 	public Collision(String[] splitRead) {
 		this.fXCoord = Integer.parseInt(splitRead[0]);
 		this.fYCoord = Integer.parseInt(splitRead[1]);
+		//TODO
 	}
 	
 	/**
 	 * Gets x-coordinate for an intersection
 	 * @return fXCoord - The x-coordinate
 	 */
-	public float xCoord() {
+	public float getXCoord() {
 		return fXCoord;
 	}
 	
@@ -35,7 +42,23 @@ public class Collision{
 	 * Gets y-coordinate for an intersection
 	 * @return fYCoord - The y-coordinate
 	 */
-	public float yCoord() {
+	public float getYCoord() {
 		return fYCoord;
+	}
+
+	/** 
+	 * Gets description of collision
+	 * @return fColDesc - collision description
+	 */
+	public String getColDesc(){
+		return fColDesc;
+	}
+
+	/**
+	 * Gets collision type
+	 * @return fColType - collision type
+	 */
+	public String getColType(){
+		return fColType;
 	}
 }
