e9da9a6f7bc87885bc7a59009353f81d69a4a429
Benson Hall <hallb8@mcmaster.ca>
2019-03-31 16:20:21 -0400

    Just boring stuff changed

--- Group08_FinalProject/app/src/main/java/DijkstraSP.java
+++ Group08_FinalProject/app/src/main/java/DijkstraSP.java
@@ -4,7 +4,6 @@
  * The shortest path is the safest path.based on a "score" for collisions.
  * A score is derived by the severity, fatalities, severe and minor injuries
  * in former collisions on the path, with precedence in the order given.
- * 
  */
 public class DijkstraSP{
 
