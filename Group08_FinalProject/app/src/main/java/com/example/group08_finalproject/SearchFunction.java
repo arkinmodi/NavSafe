e7a835567f720680e6a3dbbc38f499a3110d9962
Timothy Choy <choyt2@mcmaster.ca>
2019-04-01 01:21:12 -0400

    Update Frontend 2

--- Group08_FinalProject/app/src/main/java/com/example/group08_finalproject/SearchFunction.java
+++ Group08_FinalProject/app/src/main/java/com/example/group08_finalproject/SearchFunction.java
@@ -2,12 +2,26 @@ package com.example.group08_finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SearchFunction extends AppCompatActivity {
+    private EditText start;
+    private EditText dest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_function);
+
+        start = (EditText) findViewById(R.id.editText);
+        dest = (EditText) findViewById(R.id.editText2);
+    }
+
+    public void readInfo(View view) {
+        String s = start.getText().toString();
+        String d = dest.getText().toString();
+
+        
     }
 }
