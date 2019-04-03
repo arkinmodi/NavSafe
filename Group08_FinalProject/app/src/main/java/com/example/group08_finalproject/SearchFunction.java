3352a2ade17c064842e7a3aae91224511c859b4d
Timothy Choy <choyt2@mcmaster.ca>
2019-04-03 13:45:50 -0400

    Updating frontend 3

--- Group08_FinalProject/app/src/main/java/com/example/group08_finalproject/SearchFunction.java
+++ Group08_FinalProject/app/src/main/java/com/example/group08_finalproject/SearchFunction.java
@@ -1,13 +1,16 @@
 package com.example.group08_finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class SearchFunction extends AppCompatActivity {
     private EditText start;
     private EditText dest;
+    private static final String LOG_TAG = SearchFunction.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +22,11 @@ public class SearchFunction extends AppCompatActivity {
     }
 
     public void readInfo(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
         String s = start.getText().toString();
         String d = dest.getText().toString();
 
-        
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
