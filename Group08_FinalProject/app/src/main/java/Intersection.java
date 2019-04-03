753e6a8c21099c624a21eb5dbb93a387b8c9b6ab
Leon <leon_so7@hotmail.com>
2019-04-03 16:51:25 -0400

    update

--- Group08_FinalProject/app/src/main/java/Intersection.java
+++ Group08_FinalProject/app/src/main/java/Intersection.java
@@ -2,6 +2,8 @@
  * An ADT for Intersection Data for Seattle
  */
 public class Intersection{
+	private float fXCoord;		//X-coordinate of intersection location
+	private float fYCoord;		//Y-coordinate of intersection location
 	private String fUnitDesc;	//Intersection description
 	private String fStreetNS;	//Street in north-south direction
 	private String fStreetEW;	//Street in east-west direction
@@ -13,11 +15,31 @@ public class Intersection{
 	 * @param splitRead - array of intersections data from read module
 	 */
 	public Intersection(String[] splitRead) {
+		this.fXCoord = Float.parseFloat(splitRead[0]);
+		this.fYCoord = Float.parseFloat(splitRead[1]);
 		this.fUnitDesc = splitRead[10];
 		this.fStreetNS = fUnitDesc.split(" AND ")[0];
 		this.fStreetEW = fUnitDesc.split(" AND ")[1];
 	}
 
+	/**
+	 * Gets x-coordinate for an intersection
+	 * 
+	 * @return fXCoord - The x-coordinate
+	 */
+	public float getXCoord() {
+		return fXCoord;
+	}
+	
+	/**
+	 * Gets y-coordinate for an intersection
+	 * 
+	 * @return fYCoord - The y-coordinate
+	 */
+	public float getYCoord() {
+		return fYCoord;
+	}
+	
 	/** 
 	 * Gets description of intersection
 	 * 
